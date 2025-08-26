23c99bacf62bc168a2544cd6fd9ac82ab3550bdb
Philimuhire <philimuhire@gmail.com>
2025-08-26 22:28:48 +0200

    integrated authorization

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 
+const token = localStorage.getItem("token");
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
+    ...(token && { Authorization: `Bearer ${token}` }),
   },
 });
 
