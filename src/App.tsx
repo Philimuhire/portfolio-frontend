e5d61b3ca519b41a561ed156515be9dc25309536
Philimuhire <philimuhire@gmail.com>
2025-08-26 22:29:47 +0200

    added admin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-// src/App.tsx
-import { Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { ToastProvider, ToastViewport } from "@/components/Toast";
 import { Toaster } from "@/components/Toaster";
-import Navbar from './components/Navbar';
+import Navigation from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -11,13 +11,25 @@ import Blogs from './pages/Blogs';
 import Projects from './pages/Projects';
 import Services from './pages/Services';
 import Skills from './pages/Skills';
+import AdminPage from './pages/Admin';
+import LoginPage from './pages/LoginPage';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setToken(null); 
+  };
+
   return (
-    <>
-     <ToastProvider>
+    <ToastProvider>
       <Toaster />
-      <Navbar/>
+      <Navigation onLogout={handleLogout} token={token} />
+
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -26,11 +38,23 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/services" element={<Services />} />
         <Route path="/skills" element={<Skills />} />
+
+        <Route
+          path="/login"
+          element={token ? <Navigate to="/admin" replace /> : <LoginPage setToken={setToken} />}
+        />
+
+        <Route
+          path="/admin"
+          element={token ? <AdminPage /> : <Navigate to="/login" replace />}
+        />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-      <Footer/>
+
+      <Footer />
       <ToastViewport />
-      </ToastProvider>
-    </>
+    </ToastProvider>
   );
 }
 
